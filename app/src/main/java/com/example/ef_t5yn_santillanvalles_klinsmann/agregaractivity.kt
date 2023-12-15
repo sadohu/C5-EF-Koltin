1937c1229153a1942ffb4876118de2c16cee9139
klinsmann726 <klinsmannsantillanvalles@gmail.com>
2023-12-15 12:44:08 -0500

    test1 prueba

---
+++ app/src/main/java/com/example/ef_t5yn_santillanvalles_klinsmann/agregaractivity.kt
@@ -0,0 +1,12 @@
+package com.example.ef_t5yn_santillanvalles_klinsmann
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class agregaractivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregaractivity)
+    }
+
+}
